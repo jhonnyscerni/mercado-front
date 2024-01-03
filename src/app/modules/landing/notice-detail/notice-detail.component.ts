e421dbec1f3e3fb307d7cd12d25c6aa01d44d46d
jhonnyscerni <jhonnyscerni@gmail.com>
2024-01-03 10:12:35 -0300

    ajustes de link de botoes

--- src/app/modules/landing/notice-detail/notice-detail.component.ts
+++ src/app/modules/landing/notice-detail/notice-detail.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'app/core/auth/auth.service';
 import { Notice } from 'app/models/notice';
+import { EditalArquivoResponse } from 'app/models/notice-file';
+import { NoticeFileService } from 'app/services/notice-file.service';
 import { NoticeService } from 'app/services/notice.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-notice-detail',
@@ -17,12 +20,14 @@ export class NoticeDetailComponent implements OnInit {
   id: number
   edital: Notice;
   editalId : number
+  editalArquivo: EditalArquivoResponse;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private editaService: NoticeService,
-    private authService: AuthService) { }
+    private authService: AuthService,
+    private uploadService: NoticeFileService,) { }
 
 
     ngOnInit() {
@@ -37,6 +42,7 @@ export class NoticeDetailComponent implements OnInit {
             });
         }
     });
+    this.onRefreshArquivoResponse()
   }
 
   usuarioLogado(): boolean {
@@ -44,8 +50,40 @@ export class NoticeDetailComponent implements OnInit {
     return this.authService.isAuthenticated();
   }
 
+  onRefreshArquivoResponse(): any{
+    const $editalArquivo = this.uploadService.loadByID(this.editalId);
+    $editalArquivo.subscribe(editalArquivo => {
+      this.editalArquivo = editalArquivo;
+    }, error => this.editalArquivo = new EditalArquivoResponse());
+  }
+
+  onDownload() {
+    this.uploadService
+      .getFileFromApi(this.editalId)
+      .pipe(take(1))
+      .subscribe((response) => {
+          const downloadLink = document.createElement('a');
+          downloadLink.href = URL.createObjectURL(new Blob([response.body], { type: response.body.type }));
+  
+          const contentDisposition = response.headers.get('content-disposition');
+          const fileName = contentDisposition.split(';')[1].split('filename')[1].split('=')[1].trim();
+          downloadLink.download = fileName;
+          downloadLink.click();
+      });  
+  }
+  
+
   onLance(id): any {
     this.router.navigate(['../../editais', id ,'leilao', 'lance' ], { relativeTo: this.route });
   }
+
+  onDashboard(): any {
+    this.router.navigate(['/usuarios/lista']);
+}
+
+logout(): any {
+  this.authService.signOut();
+  this.router.navigate(['/']);
+}
 
 }
