e192378010ec54de8a8e8f84aa2ce7cbbca7b626
jhonnyscerni <jhonnyscerni@gmail.com>
2024-01-09 10:33:40 -0300

    mudanca de IP do AWS ECS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    urlbase: 'http://13.59.208.134:8080',
+    urlbase: 'http://18.191.244.222:8080',
     imagensUrl: 'http://localhost:8080',
 };
