9054ec997777e5923d8cd391bf5dfc457723197d
jhonnyscerni <jhonnyscerni@gmail.com>
2024-01-24 14:22:25 -0300

    ajuste de IP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    urlbase: 'http://3.16.163.195/:8080',
+    urlbase: 'http://3.16.163.195:8080',
     imagensUrl: 'http://localhost:8080',
 };
